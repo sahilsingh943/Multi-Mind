5ac2956bafed9787bd8f737911a09008eab45bbe
sahilsingh943 <sahiltanwar943@gmail.com>
2026-05-29 23:35:48 +0530

    third agent

--- pipeline.py
+++ pipeline.py
@@ -16,3 +16,17 @@ def run_research_pipeline(topic : str) -> dict:
     state["search_results"] = search_result['messages'][-1].content
 
     print("\n search result ",state['search_results'])
+
+
+    print("\n"+" ="*50)
+    print("step 2 - Reader agent is scraping top resources ...")
+    print("="*50)
+
+    reader_agent = build_reader_agent()
+    reader_result = reader_agent.invoke({
+        "messages": [("user",
+            f"Based on the following search results about '{topic}', "
+            f"pick the most relevant URL and scrape it for deeper content.\n\n"
+            f"Search Results:\n{state['search_results'][:800]}"
+        )]
+    })
