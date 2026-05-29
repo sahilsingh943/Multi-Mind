f9bb3113742ae476bd8c0d7beccc122567cf5b85
sahilsingh943 <sahiltanwar943@gmail.com>
2026-05-29 23:34:15 +0530

    second agent

--- pipeline.py
+++ pipeline.py
@@ -8,3 +8,11 @@ def run_research_pipeline(topic : str) -> dict:
     print("\n"+" ="*50)
     print("step 1 - search agent is working ...")
     print("="*50)
+
+    search_agent = build_search_agent()
+    search_result = search_agent.invoke({
+        "messages" : [("user", f"Find recent, reliable and detailed information about: {topic}")]
+    })
+    state["search_results"] = search_result['messages'][-1].content
+
+    print("\n search result ",state['search_results'])
