d8e6828d953fdf52eec51b030a11b73337e3104c
sahilsingh943 <sahiltanwar943@gmail.com>
2026-06-01 21:29:38 +0530

    scraped

--- pipeline.py
+++ pipeline.py
@@ -30,3 +30,25 @@ def run_research_pipeline(topic : str) -> dict:
             f"Search Results:\n{state['search_results'][:800]}"
         )]
     })
+
+    state['scraped_content'] = reader_result['messages'][-1].content
+
+    print("\nscraped content: \n", state['scraped_content'])
+
+    #step 3 - writer chain 
+
+    print("\n"+" ="*50)
+    print("step 3 - Writer is drafting the report ...")
+    print("="*50)
+
+    research_combined = (
+        f"SEARCH RESULTS : \n {state['search_results']} \n\n"
+        f"DETAILED SCRAPED CONTENT : \n {state['scraped_content']}"
+    )
+
+    state["report"] = writer_chain.invoke({
+        "topic" : topic,
+        "research" : research_combined
+    })
+
+    print("\n Final Report\n",state['report'])
